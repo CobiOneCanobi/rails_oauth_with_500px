5822a00f753a4f2c2ba7900e1dc8de09b10936dc
Cobi <cobimoos100@gmail.com>
2016-06-17 21:19:55 -0400

    code cleanup

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,14 +2,14 @@ class PhotosController < ApplicationController
   require 'fivehundredpx_api'
   require 'xoauth'
 
-
   def index
     response = FivehundredpxApi.new
+    get_photos = response.popular_photos
+    @photos = get_photos["photos"]
+
     if signed_in?
       @signed_in = true
     end
-    get_photos = response.popular_photos
-    @photos = get_photos["photos"]
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,28 +8,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # response = Xoauth.new
-    # access_token = response.generate_access_token(params[:username], params[:password])
     Xoauth2.generate_access_token(params[:username], params[:password] )
     access_token = Xoauth2.get_access_token
+
     if access_token
-      # access_token1 =Base64.decode64(access_token)
-      # ApiKey.create(access_token: access_token1).save
-      # ApiKey.new.merge(access_token)
       redirect_to photos_path
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
-    # user = User.find_by_email(params[:email])
-    # if user && user.authenticate(params[:password])
-    #   session[:user] = user.id
-    #   sign_in user
-    #   redirect_to user
-    # else
-    #   flash.now[:error] = 'Invalid email/password combination'
-    #   render 'new'
-    # end
   end
 
   def destroy
