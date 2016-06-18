0a7ab10ed96bd19f52ccc1bd9212ba5daaa4f317
Cobi <cobimoos100@gmail.com>
2016-06-18 12:24:09 -0400

    when connected to API, user can successfully vote on photos

--- lib/xoauth2.rb
+++ lib/xoauth2.rb
@@ -3,11 +3,12 @@ class Xoauth2
   require 'oauth'
 
   @@access_token = {}
+  @base_uri = 'https://api.500px.com'
 
   def self.generate_access_token(username, password)
     p "get_access_token: Initializing Consumer"
     consumer = OAuth::Consumer.new(ENV["fivehundredpx_consumer_key"], ENV["fivehundredpx_secret_key"], {
-    :site               => 'https://api.500px.com',
+    :site               => @base_uri,
     :request_token_path => "/v1/oauth/request_token",
     :access_token_path  => "/v1/oauth/access_token",
     :authorize_path     => "/v1/oauth/authorize"})
@@ -24,4 +25,23 @@ class Xoauth2
   def self.get_access_token()
     @@access_token
   end
+
+  def self.get_photos_with_votes
+    response = @@access_token.get("https://api.500px.com/v1/photos?feature=popular&sort=times_viewed&rpp=100&image_size=3&include_states=voted").body
+    response
+  end
+
+  def self.vote(photo_id)
+    response = @@access_token.post("https://api.500px.com/v1/photos/#{photo_id}/vote?vote=1")
+    hashed_response = response.to_hash
+
+    if hashed_response['status'][0] == '200 OK'
+      return true
+    else
+      return false
+    end
+  end
 end
+
+
+ # access_token = Xoauth2.instance_eval {@@access_token}
